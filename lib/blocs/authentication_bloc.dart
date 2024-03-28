9d1833247214eb6e031b2eceb2657b90ef4187ea
Prateek Thakur <prateekthakur272@gmail.com>
2024-03-28 17:30:30 +0530

    add auth bloc

---
+++ lib/blocs/authentication_bloc.dart
@@ -0,0 +1,36 @@
+import 'package:equatable/equatable.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class AuthenticationState extends Equatable {
+  @override
+  List<Object?> get props => [];
+}
+
+class AuthenticationStateAuthenticated extends AuthenticationState {
+  @override
+  List<Object?> get props => [];
+}
+
+class AuthenticationStateUnauthenticated extends AuthenticationState {
+  @override
+  List<Object?> get props => [];
+}
+
+class AuthenticationEvent extends Equatable {
+  @override
+  List<Object?> get props => [];
+}
+
+class LoadAuthenticationState extends AuthenticationEvent {}
+
+class UserChanges extends AuthenticationEvent {}
+
+class AuthenticationBloc
+    extends Bloc<AuthenticationEvent, AuthenticationState> {
+  AuthenticationBloc() : super(AuthenticationStateUnauthenticated()) {
+    on<LoadAuthenticationState>((event, emit) {
+      emit(AuthenticationStateAuthenticated());
+    });
+    on<UserChanges>((event, emit) {});
+  }
+}
